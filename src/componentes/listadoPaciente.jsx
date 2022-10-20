a19a2fb8fdb5452787531497053d42f75538d0ee
Adriana <adrianagb2105@gmail.com>
2022-10-20 18:16:59 -0500

    se modificaron los componentesformulario,listadoPaciente,paciente,Appp
    se elimino codigo de listado paciente y se agrego una funcion, en formulario quitamos un import agregamos funcionalidad, en app agregamos  props dentro de listadopaciente.

--- src/componentes/listadoPaciente.jsx
+++ src/componentes/listadoPaciente.jsx
@@ -1,20 +1,19 @@
+import Paciente from "./pacientes";
 
-  function ListadoPaciente(){
+  function ListadoPaciente({pacientes,setPaciente}){
+  //console.log(paciente)
   return (
     <div className="md:w-1/2 lg:w-3/5 overflow-y-scroll md:h-screen box-sizzing:border-box">
       <h2 className="font-black text-3xl text-center mt-5 ">Listado Paciente</h2>
       <p className="text-xl mb-10 mt-5 text-center ">
         Administra tus <span className="text-indigo-600 font-bold  ">Pacientes/Citas</span>{" "}
       </p>
-      <div className="mt-3 py-10 rounded-xl bg-slate-300 shadow-md px-5">
-        <p className="font-bold mb-3 text-gray-700 ">Nombre: <span className="font-normal normal-case">Chocolata</span></p>
-        <p className="font-bold mb-3 text-gray-700 ">Propietario: <span className="font-normal normal-case">Adriana</span></p>
-        <p className="font-bold mb-3 text-gray-700 ">Email: <span className="font-normal normal-case">adriana2105@gmail.com</span></p>
-        <p className="font-bold mb-3 text-gray-700 ">Alta: <span className="font-normal normal-case">21-05-2022</span></p>
-        <p className="font-bold mb-3 text-gray-700 ">Sintomas: <span className="font-normal normal-case">Desparacitar</span></p>
-
-
-      </div>
+      {pacientes.map(paciente=>(<Paciente 
+      key={paciente.id} paciente={paciente} setPaciente={setPaciente} />))
+      }
+      <><h2 className="font-black text-3xl text-center ">No hay Pacientes</h2>
+      <p className="text-xl tetx-center mt-5 ">Comienza Agregar Pacientes <span className="text-indigo-600 font-bold">Y apareceran en este lugar</span> </p>
+      </>
     </div>
   );
 }
